6744dad57bfb070ecaeb49d30f7ade6e771034cb
appleseed <apple12347678@gmail.com>
2020-11-22 23:49:29 +0900

    feat(favicon): add icons and favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,13 +39,50 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
-        name: "appleseed's dev blog",
+        name: "appleseed's homepage",
         short_name: 'appleseed',
         start_url: '/',
         background_color: '#ffffff',
         theme_color: '#17181c',
         display: 'minimal-ui',
-        icons: [],
+        icons: [
+          {
+            src: 'icons/android-icon-36x36.png',
+            sizes: '36x36',
+            type: 'image/png',
+            density: '0.75',
+          },
+          {
+            src: 'icons/android-icon-48x48.png',
+            sizes: '48x48',
+            type: 'image/png',
+            density: '1.0',
+          },
+          {
+            src: 'icons/android-icon-72x72.png',
+            sizes: '72x72',
+            type: 'image/png',
+            density: '1.5',
+          },
+          {
+            src: 'icons/android-icon-96x96.png',
+            sizes: '96x96',
+            type: 'image/png',
+            density: '2.0',
+          },
+          {
+            src: 'icons/android-icon-144x144.png',
+            sizes: '144x144',
+            type: 'image/png',
+            density: '3.0',
+          },
+          {
+            src: 'icons/android-icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            density: '4.0',
+          },
+        ],
       },
     },
     'gatsby-plugin-emotion',
