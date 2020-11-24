b17af20d750d86eef474aab4c630956b56a8c96f
appleseed <apple12347678@gmail.com>
2020-11-24 19:18:33 +0900

    feature(404): add 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import styled from '@emotion/styled';
+import { useTranslation } from 'react-i18next';
+
+import { SEO } from '../components';
+
+const Wrapper = styled.div`
+  padding: 1.5rem 2rem;
+`;
+
+export default function NotFoundPage() {
+  const { t } = useTranslation();
+  return (
+    <>
+      <SEO title="Not found" />
+      <Wrapper>
+        <h1>404: Not Found</h1>
+        <p>{t('404.message')}</p>
+      </Wrapper>
+    </>
+  );
+}
